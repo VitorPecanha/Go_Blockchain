d8c5bd51a00267666926989644843f6d2c715537
Vitor Peçanha <vitor.pecanha@hotmail.com>
2022-06-08 00:05:59 -0300

    Created the AddBlock function

--- main.go
+++ main.go
@@ -34,3 +34,9 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 type Blockchain struct {
 	blocks []*Block
 }
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}

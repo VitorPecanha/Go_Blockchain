d3cf8d69a6def0380220c9dec70af3bc491f872b
Vitor Peçanha <vitor.pecanha@hotmail.com>
2022-06-08 00:03:52 -0300

    Created the Blockchain struct

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"strconv"
+	"time"
 )
 
 //Simplified version of a Blockchain's block
@@ -14,10 +15,22 @@ type Block struct {
 	Hash          []byte
 }
 
+//Function to calculate the hash
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
+}
+
+//Function that simplify the block creation
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+type Blockchain struct {
+	blocks []*Block
 }
